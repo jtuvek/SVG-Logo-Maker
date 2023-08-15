83d93b7f667eefca57e8b1fe4e54c26386d8410e
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-08-14 22:45:58 -0400

    fixed code

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const { getUserInput } = require("./lib/userInput");
 const { generateSVG } = require("./lib/svgFileHandler");
-const { saveSVGToFile } = require("./lib/svgFileHandler"); // Import the saveSVGToFile function
 
 (async () => {
   const userInput = await getUserInput();
 
-  const svgContent = generateSVG(userInput.shape, userInput.shapeColor, userInput.text, userInput.textColor);
+  generateSVG(userInput.shape, userInput.shapeColor, userInput.text, userInput.textColor);
 
-  const filename = "logo.svg";
-  saveSVGToFile(svgContent, filename); // Save SVG content to file
-
-  console.log("Generated " + filename);
+  console.log("Generated logo.svg");
 })();
